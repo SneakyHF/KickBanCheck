8d929f668e4497ace6a3d4449363d67b336d45d6
$neaky <135932795+SneakyHF@users.noreply.github.com>
2023-08-19 17:41:28 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -57,29 +57,73 @@ function renderUserInfo(data) {
 }
 
 function checkBanStatus() {
-    clearUserInfo();
+    clearUserInfo(); // Clear previous user info
 
-    const username = usernameInput.value;
+    const username = document.getElementById("username").value;
     const url = "https://kick.com/emotes/" + encodeURIComponent(username);
     const xhr = new XMLHttpRequest();
+
+    const userInfoElement = document.getElementById("userInfo");
 
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 const data = JSON.parse(xhr.responseText);
+                const banStatus = document.getElementById("banStatus");
 
                 if (Array.isArray(data) && data.length > 0) {
                     const isBanned = data[0].is_banned;
 
-                    updateBanStatus(isBanned);
-                    renderUserInfo(data);
+                    banStatus.textContent = isBanned ? "Banned" : "OK";
+
+                    // Display user info
+                    if (data[0].user) {
+                        userInfoElement.innerHTML = `
+                            <center><img src="${data[0].user.profile_pic}" alt="Profile Picture"></center>
+                            <br>
+                            <center><p class="bio"><i>${data[0].user.bio}</i></p></center>
+                            <br>
+                            <p><b>User ID</b>: ${data[0].user_id}</p>
+                            <p><b>Subscription Enabled</b>: ${data[0].subscription_enabled}</p>
+                            <p><b>VOD Enabled</b>: ${data[0].vod_enabled}</p>
+                            <p><b>Can Host</b>: ${data[0].can_host}</p>
+                            <div class="social-links">
+                                ${data[0].user.discord ? `<a href="https://discord.gg/${data[0].user.discord}">Discord</a>` : ''}
+                                ${data[0].user.facebook ? `<a href="https://facebook.com/${data[0].user.facebook}">Facebook</a>` : ''}
+                                ${data[0].user.instagram ? `<a href="https://instagram.com/${data[0].user.instagram}">Instagram</a>` : ''}
+                                ${data[0].user.tiktok ? `<a href="https://tiktok.com/@${data[0].user.tiktok}">TikTok</a>` : ''}
+                                ${data[0].user.twitter ? `<a href="https://twitter.com/${data[0].user.twitter}">Twitter</a>` : ''}
+                            </div>
+                            <br>
+                            <button id="playbackButton" class="playback-button" onclick="copyPlaybackURL('${data[0].playback_url}')">Copy Playback URL</button>
+                        `;
+
+                        // Show the user info div
+                        showUserInfo();
+                    } else {
+                        // No user data found
+                        userInfoElement.textContent = "No user data found.";
+                    }
+
+                    // Update the ban status color
+                    banStatus.classList.toggle("green", !isBanned);
+                    banStatus.classList.toggle("red", isBanned);
                 } else {
+                    // No data found for the user
                     banStatus.textContent = "No data found";
+                    banStatus.classList.remove("green", "red"); // Remove any previous styling
+
+                    // Hide the user info div
                     hideUserInfo();
                 }
             } else {
+                // Error handling when request fails
+                const banStatus = document.getElementById("banStatus");
                 banStatus.textContent = "User not found";
+                banStatus.classList.remove("green", "red"); // Remove any previous styling
+
+                // Hide the user info div
                 hideUserInfo();
             }
         }
