47ae680f8cceaf1e1eea22d9d76d7606e2df2cdc
$neaky <135932795+SneakyHF@users.noreply.github.com>
2023-08-19 16:34:23 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -39,9 +39,30 @@ function checkBanStatus() {
                     banStatus.classList.toggle("green", !isBanned);
                     banStatus.classList.toggle("red", isBanned);
 
-                    // Display user info
+                   // Display user info
                     userInfoElement.innerHTML = `
-                        <!-- ... (rest of the code) ... -->
+                        <center><img src="${data[0].user.profile_pic}" alt="Profile Picture"></center>
+                        <br>
+                        <center><p class="bio"><i>${data[0].user.bio}</i></p></center>
+                        <br>
+                        <p><b>User ID</b>: ${data[0].user_id}</p>
+                        <p><b>Subscription Enabled</b>: ${data[0].subscription_enabled}</p>
+                        <p><b>VOD Enabled</b>: ${data[0].vod_enabled}</p>
+                        <p><b>Can Host</b>: ${data[0].can_host}</p>
+                        <div class="social-links">
+                            ${data[0].user.discord ? `<a href="https://discord.gg/${data[0].user.discord}">Discord</a>` : ''}
+                            ${data[0].user.facebook ? `<a href="https://facebook.com/${data[0].user.facebook}">Facebook</a>` : ''}
+                            ${data[0].user.instagram ? `<a href="https://instagram.com/${data[0].user.instagram}">Instagram</a>` : ''}
+                            ${data[0].user.tiktok ? `<a href="https://tiktok.com/@${data[0].user.tiktok}">TikTok</a>` : ''}
+                            ${data[0].user.twitter ? `<a href="https://twitter.com/${data[0].user.twitter}">Twitter</a>` : ''}
+                            ${
+                                data[0].user.youtube
+                                    ? generateYouTubeLink(data[0].user.youtube)
+                                    : ''
+                            }
+                        </div>
+                        <br>
+                        <button id="playbackButton" class="playback-button" onclick="copyPlaybackURL('${data[0].playback_url}')">Copy Playback URL</button>
                     `;
 
                     playbackButton.textContent = "Copy Playback URL";
