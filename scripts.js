078e69b916a9b8325bb96896f61c2afe5297b1f0
$neaky <135932795+SneakyHF@users.noreply.github.com>
2023-08-19 18:03:12 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,50 +40,44 @@ function checkBanStatus() {
             if (xhr.status === 200) {
                 const data = JSON.parse(xhr.responseText);
 
-                if (Array.isArray(data) && data.length > 0) {
-                    const isBanned = data[0].is_banned;
+                const isBanned = data[0].is_banned;
 
-                    banStatus.textContent = isBanned ? "Banned" : "OK";
-                    banStatus.classList.add(isBanned ? "red" : "green");
-                    banStatus.classList.remove(isBanned ? "green" : "red");
+                banStatus.textContent = isBanned ? "true" : "false";
+                banStatus.classList.add(isBanned ? "red" : "green");
+                banStatus.classList.remove(isBanned ? "green" : "red");
 
-                    if (data[0].user) {
-                        // Display user info
-                        userInfoElement.innerHTML = `
-                            <center><img src="${data[0].user.profile_pic}" alt="Profile Picture"></center>
-                            <br>
-                            <center><p class="bio"><i>${data[0].user.bio}</i></p></center>
-                            <br>
-                            <p><b>User ID</b>: ${data[0].user_id}</p>
-                            <p><b>Subscription Enabled</b>: ${data[0].subscription_enabled}</p>
-                            <p><b>VOD Enabled</b>: ${data[0].vod_enabled}</p>
-                            <p><b>Can Host</b>: ${data[0].can_host}</p>
-                            <div class="social-links">
-                                ${data[0].user.discord ? `<a href="https://discord.gg/${data[0].user.discord}">Discord</a>` : ''}
-                                ${data[0].user.facebook ? `<a href="https://facebook.com/${data[0].user.facebook}">Facebook</a>` : ''}
-                                ${data[0].user.instagram ? `<a href="https://instagram.com/${data[0].user.instagram}">Instagram</a>` : ''}
-                                ${data[0].user.tiktok ? `<a href="https://tiktok.com/@${data[0].user.tiktok}">TikTok</a>` : ''}
-                                ${data[0].user.twitter ? `<a href="https://twitter.com/${data[0].user.twitter}">Twitter</a>` : ''}
-                                ${
-                                    data[0].user.youtube
-                                        ? generateYouTubeLink(data[0].user.youtube)
-                                        : ''
-                                }
-                            </div>
-                            <br>
-                            <button id="playbackButton" class="playback-button" onclick="copyPlaybackURL('${data[0].playback_url}')">Copy Playback URL</button>
-                        `;
-                        
-                        showUserInfo();
-                    } else {
-                        // No user data found
-                        userInfoElement.innerHTML = `<p>No user data found</p>`;
-                        showUserInfo();
-                    }
+                if (data[0].user) {
+                    // Display user info
+                    userInfoElement.innerHTML = `
+                        <center><img src="${data[0].user.profile_pic}" alt="Profile Picture"></center>
+                        <br>
+                        <center><p class="bio"><i>${data[0].user.bio}</i></p></center>
+                        <br>
+                        <p><b>User ID</b>: ${data[0].user_id}</p>
+                        <p><b>Subscription Enabled</b>: ${data[0].subscription_enabled}</p>
+                        <p><b>VOD Enabled</b>: ${data[0].vod_enabled}</p>
+                        <p><b>Can Host</b>: ${data[0].can_host}</p>
+                        <div class="social-links">
+                            ${data[0].user.discord ? `<a href="https://discord.gg/${data[0].user.discord}">Discord</a>` : ''}
+                            ${data[0].user.facebook ? `<a href="https://facebook.com/${data[0].user.facebook}">Facebook</a>` : ''}
+                            ${data[0].user.instagram ? `<a href="https://instagram.com/${data[0].user.instagram}">Instagram</a>` : ''}
+                            ${data[0].user.tiktok ? `<a href="https://tiktok.com/@${data[0].user.tiktok}">TikTok</a>` : ''}
+                            ${data[0].user.twitter ? `<a href="https://twitter.com/${data[0].user.twitter}">Twitter</a>` : ''}
+                            ${
+                                data[0].user.youtube
+                                    ? generateYouTubeLink(data[0].user.youtube)
+                                    : ''
+                            }
+                        </div>
+                        <br>
+                        <button id="playbackButton" class="playback-button" onclick="copyPlaybackURL('${data[0].playback_url}')">Copy Playback URL</button>
+                    `;
+                    
+                    showUserInfo();
                 } else {
-                    // No data found for the user
-                    banStatus.textContent = "No data found";
-                    hideUserInfo();
+                    // No user data found
+                    userInfoElement.innerHTML = `<p>No user data found</p>`;
+                    showUserInfo();
                 }
             } else {
                 // Error handling when request fails
